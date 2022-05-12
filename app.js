722381c39d0932cf1b323847c12de7e2916ba6e0
Edgardo-G <80786200+Edgardo-G@users.noreply.github.com>
2022-05-12 20:05:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const dbConnect = require('./config/mongo')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.use('/api', require('./routes'))
+
+app.listen(port, () => {
+    console.log('Tu app esta lista por http://localhost:'+port)
+})
+
+dbConnect()
